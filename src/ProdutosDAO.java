daebb0e0864a841bc66c69269ebcaa6015eff2ad
Gustavo Tibes de Oliveira <gustavotibes8@gmail.com>
2026-09-21 21:09:44 -0300

    filtro para produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -87,6 +87,37 @@ public class ProdutosDAO {
         return true;
     
     }
+   
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
     
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos as p WHERE p.status = 'Vendido'";
+        
+        try {
+            
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+             
+            }
+            
+        }
+        catch (SQLException sqle) {
+            System.out.println(sqle);
+        }
+        
+        return listagem;
+    
+    }
+
 }
 
